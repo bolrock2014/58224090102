0c3767e93786d898a14efaf30c63cdcf7eaf1d7a
bolrock2014 <offza_za00@hotmail.com>
2017-09-03 15:57:14 +0700

    add3-13

---
+++ 3-13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int numder = 25;
+	int a=1,d=2,c=3;
+	float real = 99.99;
+	float point1=45.2, point2=30;
+	char choice = 'a';
+	char ch1='o', ch2='a';
+	printf("numder = %d\n",numder);
+	printf("a=%d b=%d c=%d\n",1,2,3);
+	printf("real=%.2f\n",real);
+	printf("point=%.1f point2=%.0f\n",point1,point2);
+	printf("choice=%c\n",choice);
+	printf("ch1=%c ch2=%c",ch1,ch2);
+	
+}
