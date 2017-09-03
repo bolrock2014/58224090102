b25f1e706d5aae72a30491a9274286cafaef4d90
bolrock2014 <offza_za00@hotmail.com>
2017-09-03 15:47:17 +0700

    Initial commit

---
+++ 02.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int numder = 25;
+	int a=1,d=2,c=3;
+	float real = 99.99;
+	float point1=45.2, point2=30;
+	char choice = 'a';
+	char vh1='0', ch2='a';
+	printf("%d\n",numder);
+}
