3affaf3ff748d10fead8821bca3de79746d58679
bolrock2014 <31584748+bolrock2014@users.noreply.github.com>
2017-11-20 12:08:28 +0700

    Add files via upload
    ส่งงาน กรรชัย พรธรรปปรีชา

--- 3-13.c
+++ 3-13.c
@@ -1,19 +1,12 @@
 #include<stdio.h>
-void main()
+int main()
 {
-	int numder = 25;
-	int a=1,d=2,c=3;
-	float real = 99.99;
-	float point1=45.2, point2=30;
-	char choice = 'a';
-	char ch1='o', ch2='z';
-	printf("numder = %d\n",numder);
-	printf("a=%d b=%d c=%d\n",1,2,3);
-	printf("real=%.2f\n",real);
-	printf("point=%.1f point2=%.0f\n",point1,point2);
-	printf("choice=%c\n",choice);
-	printf("ch1=%c ch2=%c\n",ch1,ch2);
-	
-	long _Numder2 = 40000;
-	printf("numder2=%d",_Numder2);
+	int		number=25;
+	int		a=1,b=2,c=3;
+	float	real = 99.99;
+	float	pointl=45.2,point2=30;
+	char	choice='a';
+	char	ch1='o',ch2='z';
+	printf("number = %d\n",number);
+	return 0;
 }
